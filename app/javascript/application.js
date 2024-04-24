7fbc6c3bd5ab8ffba91227d2971cd338e2b03427
Edi Edwardson C. Meer <edison.cmeer@gmail.com>
2024-04-24 21:21:05 +0800

    Added Navigation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//= require jquery
+//= require semantic-ui
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+})
